0e0aa3af422310fe941b588aeaca40c2e720f85f
HopKaa <127760062+HopKaa@users.noreply.github.com>
2023-06-06 21:01:22 +0700

    UI creating

---
+++ Assets/Scripts/FigureController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FigureController : MonoBehaviour
+{
+    public float lifeTime; // Время жизни фигуры
+    public float timer; // Таймер фигуры
+
+    private void Start()
+    {
+        // Задаем случайное время жизни
+        lifeTime = Random.Range(2f, 5f);
+        // Инициализируем таймер
+        timer = lifeTime;
+    }
+
+    private void Update()
+    {
+        // Уменьшаем таймер фигуры каждый кадр
+        timer -= Time.deltaTime;
+        // Если таймер дошел до нуля, уничтожаем фигуру
+        if (timer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
